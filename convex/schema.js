f91ee929763049c62c6fcb77488b282d9a82b244
VaibhavTaragi <taragivaibhav@gmail.com>
2026-01-01 22:19:01 +0530

    Landing Page Done

---
+++ convex/schema.js
@@ -0,0 +1,51 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  users: defineTable({
+    name: v.string(),
+    email: v.string(),
+    tokenIdentifier: v.string(),
+    imageUrl: v.optional(v.string()),
+  })
+    .index("by_token", ["tokenIdentifier"])
+    .index("by_email", ["email"])
+    .searchIndex("search_name", { searchField: "name" })
+    .searchIndex("search_email", { searchField: "email" }),
+
+  expenses: defineTable({
+    description: v.string(),
+    amount: v.number(),
+    category: v.optional(v.string()),
+    date: v.number(), //timestamp
+    paidByUserId: v.id("users"), //reference to users table
+    splitType: v.string(), //"equal", "percentage","exact"
+    splits: v.array(
+      v.object({
+        userId: v.id("users"), //Refernce to users table
+        amount: v.number(), // amount owed by this user
+        paid: v.boolean(),
+      })
+    ),
+    groupId: v.optional(v.id("groups")), // reference to  groups table, undefined  one-on-one expenses
+    createdBy: v.id("users"),
+  })
+    .index("by_group", ["groupId"])
+    .index("by_user_and_group", ["paidByUserId", "groupId"])
+    .index("by_date", ["date"]),
+
+  settlements: defineTable({
+    amount: v.number(),
+    note: v.optional(v.string()),
+    date: v.number(), //timestamp
+    paidByUserId: v.id("users"), //reference to users table
+    receivedByUserId: v.id("users"), //reference to users table
+    groupId: v.optional(v.id("groups")), // reference to  groups table, undefined  one-on-one expenses
+    relatedExpenseIds: v.optional(v.array(v.id("expenses"))), //Which expenses this settlement covers
+    createdBy: v.id("users"),
+  })
+    .index("by_group", ["groupId"])
+    .index("by_user_and_group", ["paidByUserId", "groupId"])
+    .index("by_receiver_and_group", ["receivedByUserId", "groupId"])
+    .index("by_date", ["date"]),
+});
